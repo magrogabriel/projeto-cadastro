27c69da9b1997e300539a5c3ed9ddb5625e25672
Gabriel Magro <gabrielmagro.94@gmail.com>
2023-01-13 19:49:53 -0300

    id type

--- src/core/Cliente.ts
+++ src/core/Cliente.ts
@@ -3,14 +3,14 @@ export default class Cliente {
     #nome: string
     #idade: number
 
-    constructor(nome: string, idade: number, id: string = null) {
+    constructor(nome: string, idade: number, id: string ) {
         this.#nome = nome
         this.#idade = idade
         this.#id = id
     }
 
     static vazio() {
-        return new Cliente('', 0)
+        return new Cliente('', 0, '')
     }
 
     get id() {
